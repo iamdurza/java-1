87ea6c548ac64c440f0004fbc8fb63eeeff724b1
Jake <durza@skilstak.sh>
2015-09-12 15:56:18 -0400

    save

---
+++ eightball.java
@@ -0,0 +1,25 @@
+import skilstak.c;
+
+public class eightball {
+    public static final void main(String[] args) {
+    System.out.print(c.cl + c.g + "Welcome to the majic 8ball!" );
+    }
+}    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
