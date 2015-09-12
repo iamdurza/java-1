2eccaa3f774e3df96d914d2c8c3ead58bf45c8bb
Jake <durza@skilstak.sh>
2015-09-12 16:27:17 -0400

    save

--- eightball.java
+++ eightball.java
@@ -1,24 +1,29 @@
 import skilstak.c;
+import java.util.Scanner;
+import java.util.List;
 
 public class eightball {
+    
+    private static final String input(String prompt) {
+        System.out.print(prompt);
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        return text;
+    }
+    
+    /*
+    private static final Object choice(List list) {   
+    }
+    */
+    
     public static final void main(String[] args) {
-    System.out.print(c.cl + c.g + "Welcome to the majic 8ball!" );
+        System.out.print(c.cl + c.g + "Welcome to the majic 8ball! Pls enter yo questions");
+        
+        while (true) {
+        String question = input("--> " + c.B3);
+        }
     }
 }    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
