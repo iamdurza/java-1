a0db6717009834bc04ce8a4e9fcf0f1865681c16
Jake <durza@skilstak.sh>
2015-09-12 16:55:55 -0400

    save

--- eightball.java
+++ eightball.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 public class eightball {
     
+    private static String[] list = {"Yes.","No.","Maybe So."};
+
     private static final String input(String prompt) {
         System.out.print(prompt);
         Scanner scanner = new Scanner(System.in);
@@ -11,16 +13,20 @@ public class eightball {
         return text;
     }
     
-    /*
-    private static final Object choice(List list) {   
+    
+    private static final String choice(String[] list) {   
+        int index = (int) (Math.random()*list.length);
+        return list[index];
     }
-    */
+    
     
     public static final void main(String[] args) {
-        System.out.print(c.cl + c.g + "Welcome to the majic 8ball! Pls enter yo questions");
+        System.out.print(c.cl + c.g + "Welcome to the magic 8ball! Pls enter yo questions");
         
         while (true) {
         String question = input("--> " + c.B3);
+        System.out.println(choice(list));
+        //System.out.println(question);
         }
     }
 }    
