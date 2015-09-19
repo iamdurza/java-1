2896990e3bebfc8b077310675a63417f5f7c46a1
Jake <durza@skilstak.sh>
2015-09-19 16:27:15 -0400

    save

--- BattleEng/Pokemon.java
+++ BattleEng/Pokemon.java
@@ -4,9 +4,10 @@ public interface Pokemon {
     public abstract int type;
     public abstract int attack;
     public abstract int specialAttak;
+    public abstract int attack;
     public abstract int SpecialDefense;
-    public abstract String[] moves;
-    public abstract String[] abilities;
+    public abstract Move[] moves;
+    public abstract Ability[] abilities;
 
 
 }
